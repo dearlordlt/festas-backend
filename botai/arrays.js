0df0ef6fe0b0dfa2be0a3c51086f6dc6abc29b64
getjonas <getjonas@gmail.com>
2016-09-28 23:35:02 +0300

    paslaptys2

--- botai/arrays.js
+++ botai/arrays.js
@@ -6,7 +6,7 @@ mod.asmuo = ['Landsbergis', 'prezidentė', 'Mockus', 'mergaitė', 'dr. Jonas', '
 
 mod.titulas = ['tamsos Valdovas', 'profesorius', 'studentas', 'amžiną atilsį', 'doc.', 'prof.', 'projekto vadovas'];
 
-mod.sexyVeiksnys = ['penis', 'vagina', 'sėkla', 'libido', 'mano mažytis', 'saldi nuodėmė', 'bumerangas', 'centrifuga', 'gerai suderintas klavyras', 'anglų ragas', 'smuikelis', 'eržilas'];
+mod.sexyVeiksnys = ['penis', 'vagina', 'sėkla', 'libido', 'mano_mažytis', 'saldi_nuodėmė', 'bumerangas', 'centrifuga', 'gerai_suderintas_klavyras', 'anglų_ragas', 'smuikelis', 'eržilas'];
 
 mod.tarinys = ['išaugo', 'nukrito', 'išvažiavo', 'daugiau didėjo nei mažėjo', 'nepritarė šiuo klausimu', 'atsirado', 'išnyko', 'sužibėjo', 'nustebino', 'nenustebino', 'išlindo', 'kreipėsi į aukštesnes institucijas'];
 
@@ -16,23 +16,23 @@ mod.polIntro = ['Per paskutinę kadenciją ', 'Apklausos rodo, kad ', 'Slaptoje 
 
 mod.polMid = ['Dabar reikalinga ', 'Tuo labiau, kai ir taip yra ', 'Šalia to - ', 'Pasižadu, mano šūkis bus ', 'Jau žinoma, kad tai buvo ', 'Tai ir yra ', 'Šitą nusikaltimą atliko ', 'Tarp kaltininkų tik ', 'Jei taip ir toliau, būtinai ateis ', 'Niekas kitas čia nekaltas, ', 'Laurus susirinko '];
 
-mod.hash = ['followme', 'hashtag', 'groteles', 'rinkimai2016', 'sirdziuprezidentas', 'food4thoughts', 'satan', 'thuglife', 'girls', 'just4fun', 'life', 'gangsta', 'ironija', 'paduduok', 'tikjuokauju', 'nesigailiunk', 'MGvalda', '106k', 'erudicija', 'sparnuotizodziai', 'midnightfewer', 'fanboy', 'YOLO', 'nosferatu', 'pasiutpolke'];
+mod.hash = ['followme', 'hashtag', 'groteles', 'rinkimai2016', 'sirdziuprezidentas', 'food4thoughts', 'satan', 'thuglife', 'girls', 'just4fun', 'life', 'gangsta', 'ironija', 'paduduok', 'tikjuokauju', 'nesigailiunieko', 'MGvalda', '106k', 'erudicija', 'sparnuotizodziai', 'midnightfewer', 'fanboy', 'YOLO', 'nosferatu', 'pasiutpolke'];
 
 mod.coolIntro = ['Aš esu kambaryje, čia tamsu ', 'Leiskite nors šiek tiek apšilti man kojas, ', 'Nebenoriu miegoti viena, todėl ', 'Kas yra verslas, ', 'Kai kuriose savivaldybėse ', 'Procentas, tai čia šimtas, '];
 
-mod.coolMid = ['dabar jau tikrai ', ' jeigu jūs klaustumėte', 'bet jau į generalinius klausimus ', 'tai jau tikrai '];
+mod.coolMid = ['dabar jau tikrai ', 'jeigu jūs klaustumėte', 'bet jau į generalinius klausimus ', 'tai jau tikrai '];
 
-mod.coolOutro = ['eksportas pasiekė prieškrizinį lygį', ' mediena nugalėtų nelygioj kovoj', 'turiu trumpų ir šmaikščių atsakymų', 'niekados nepasakysiu', ' pereiname prie biokuro', 'telefonas išsikrauna', 'viskas, trūksta kantrybė'];
+mod.coolOutro = ['eksportas pasiekė prieškrizinį lygį', 'mediena nugalėtų nelygioj kovoj', 'turiu trumpų ir šmaikščių atsakymų', 'niekados nepasakysiu', 'pereiname prie biokuro', 'telefonas išsikrauna', 'viskas, trūksta kantrybė'];
 
 mod.zenklas = ['?', '.', '!', '...'];
 
 mod.engIntro = ['Ai em šuje:', 'Die gesc', 'Leidyz end džentelmen', 'Disingiš gesc', 'Ai vud laik tju emfazaiz zet'];
 
-mod.engSomething = [' de komitment of ze Ripablik of Litiujiėnija iz ', ' ai em seitan ', ' de konstitušen of jūnesko '];
+mod.engSomething = [' de komitment of ze Ripablik of Litiujiėnija iz ', 'ai em seitan ', 'de konstitušen of jūnesko '];
 
 mod.engFatality = ['a šied vju', 'šoude tu šoude', 'of ze hjuman kaind', 'bete, smate, mo kriėitiv end chohierint fjūčia togezia'];
 
-mod.engOutro = ['. Fenk jū veri mač.', ' for it tiu bylong lasting.', ', pliz šier end laik.'];
+mod.engOutro = ['. Fenk jū veri mač.', 'for it tiu bylong lasting.', ', pliz šier end laik.'];
 
 mod.zoidberg1 = ['i`m swelling with patriotic mucus! ', 'You’ll never guess where I’ve been! ', 'Ah, there’s nothing like the first cup of dumpster juice in the morning!', 'Why always the fighting? ', 'You seem malnourished. Are you suffering from internal parasites?', 'I`m having a wonderfulime', 'Eexcuse me, but who\'s the doctor here?'];
 
@@ -40,7 +40,7 @@ mod.zoidberg2 = ['By the way, I took the liberty of fertilizing your caviar', 'I
 
 mod.grazulioPriesas = ['homoseksualistai', 'STT', 'STT agentai', 'seimo nariai', 'amerikos šnipas', 'konservatoriai'];
 
-mod.vytas = ['ubagai ', 'valdže ', 'vagiai ', 'šleževičius ', 'prie ruso ', 'darbo turėjom anksčiau ', 'odabar kas ', 'kače ', 'nu ', 'nu ', 'ponai ir ubagai ', 'VON nato ', 'lietuva ', 'Lietuvą ', 'durnas buvau nevogiau ', 'jau tada sakiau', 'kaimas išleikė Lietuvybe ', 'debilizmas ', 'skursime kol amerikos pančiai bus ', 'buožės ', 'negaliu kentėt ', 'galiu kentėt kiek tik reikia ', 'kenčiu ir kenčiu ', 'partijos visos parsidavė', 'niekad neparsidaviau', 'pinigus skurdžiams ', 'galiu pasakyt viskas bloges ', 'velniop užsienio bankus ', 'nachui ', 'kaimo debilai', 'ne durniam isvest Lietuva is skurdo', 'sudrebetu visa sistema ', ' ,, ', ' .', 'įrodymai kur', 'nuimkit arba mes nuplėšim ', 'galim pasiekt duodant į galvą ', 'liberalai apsišiko ', ' vagys ', 'eina į seima vogt ', 'aš būčiau mokėjęs vogt dabar viską turėčiau ', 'per visas televizijas prasau pasakyti ', 'kaip vagia ', 'smerkia ', 'kaltina ', 'engia ', 'rusija ', 'putinas vyras ', 'rusai isgelbės'];
+mod.vytas = ['ubagai ', 'valdže ', 'vagiai ', 'šleževičius ', 'prie ruso ', 'darbo turėjom anksčiau ', 'odabar kas ', 'kače ', 'nu ', 'nu ', 'ponai ir ubagai ', 'VON nato ', 'lietuva ', 'Lietuvą ', 'durnas buvau nevogiau ', 'jau tada sakiau', 'kaimas išleikė Lietuvybe ', 'debilizmas ', 'skursime kol amerikos pančiai bus ', 'buožės ', 'negaliu kentėt ', 'galiu kentėt kiek tik reikia ', 'kenčiu ir kenčiu ', 'partijos visos parsidavė', 'niekad neparsidaviau', 'pinigus skurdžiams ', 'galiu pasakyt viskas bloges ', 'velniop užsienio bankus ', 'nachui ', 'kaimo debilai', 'ne durniam isvest Lietuva is skurdo', 'sudrebetu visa sistema ', ',, ', '.', 'įrodymai kur', 'nuimkit arba mes nuplėšim ', 'galim pasiekt duodant į galvą ', 'liberalai apsišiko ', 'vagys ', 'eina į seima vogt ', 'aš būčiau mokėjęs vogt dabar viską turėčiau ', 'per visas televizijas prasau pasakyti ', 'kaip vagia ', 'smerkia ', 'kaltina ', 'engia ', 'rusija ', 'putinas vyras ', 'rusai isgelbės'];
 
 ////////////////////////////////////////////////////////////
 
