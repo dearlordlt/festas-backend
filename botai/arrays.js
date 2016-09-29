a9db9dd51caf5833eafb3565a844d75f9f79ba7b
ssliteris <ssliteris@deloitte.com>
2016-09-29 11:06:42 +0300

    bb

--- botai/arrays.js
+++ botai/arrays.js
@@ -1,4 +1,4 @@
-var mod = {};
+﻿var mod = {};
 
 mod.veiksnys = ['naktinė mokesčių reforma', 'valdžia', 'rinkėjas', 'vidutinė motina', 'aukštasis mokslas', 'esama padėtis', 'satanizmas', 'tolerancija', 'politika', 'emigracija', 'imigracija', 'dauguma rodiklių'];
 
@@ -6,7 +6,7 @@ mod.asmuo = ['Landsbergis', 'prezidentė', 'Mockus', 'mergaitė', 'dr. Jonas', '
 
 mod.titulas = ['tamsos Valdovas', 'profesorius', 'studentas', 'amžiną atilsį', 'doc.', 'prof.', 'projekto vadovas'];
 
-mod.sexyVeiksnys = ['penis', 'vagina', 'sėkla', ''];
+mod.sexyVeiksnys = ['penis', 'vagina', 'sėkla', 'libido', 'mano_mažytis', 'saldi_nuodėmė', 'bumerangas', 'centrifuga', 'gerai_suderintas_klavyras', 'anglų_ragas', 'smuikelis', 'eržilas'];
 
 mod.tarinys = ['išaugo', 'nukrito', 'išvažiavo', 'daugiau didėjo nei mažėjo', 'nepritarė šiuo klausimu', 'atsirado', 'išnyko', 'sužibėjo', 'nustebino', 'nenustebino', 'išlindo', 'kreipėsi į aukštesnes institucijas'];
 
@@ -16,36 +16,58 @@ mod.polIntro = ['Per paskutinę kadenciją ', 'Apklausos rodo, kad ', 'Slaptoje 
 
 mod.polMid = ['Dabar reikalinga ', 'Tuo labiau, kai ir taip yra ', 'Šalia to - ', 'Pasižadu, mano šūkis bus ', 'Jau žinoma, kad tai buvo ', 'Tai ir yra ', 'Šitą nusikaltimą atliko ', 'Tarp kaltininkų tik ', 'Jei taip ir toliau, būtinai ateis ', 'Niekas kitas čia nekaltas, ', 'Laurus susirinko '];
 
-mod.hash = [''];
+mod.hash = ['followme', 'hashtag', 'groteles', 'rinkimai2016', 'sirdziuprezidentas', 'food4thoughts', 'satan', 'thuglife', 'girls', 'just4fun', 'life', 'gangsta', 'ironija', 'paduduok', 'tikjuokauju', 'nesigailiunieko', 'MGvalda', '106k', 'erudicija', 'sparnuotizodziai', 'midnightfewer', 'fanboy', 'YOLO', 'nosferatu', 'pasiutpolke'];
 
-mod.coolIntro = ['Aš esu kambaryje, čia tamsu ', 'Leiskite nors šiek tiek apšilti man kojas, ', 'Nebenoriu miegoti viena, todėl ', 'Kas yra verslas, ', 'Kai kuriose savivaldybėse ', 'Procentas, tai čia šimtas, '];
+mod.coolIntro = ['Aš esu kambaryje, čia tamsu, todėl ', 'Leiskite nors šiek tiek apšilti man kojas, ', 'Nebenoriu miegoti viena, todėl ', 'Kas yra verslas, ', 'Kai kuriose savivaldybėse ', 'Procentas, tai čia šimtas, '];
 
-mod.coolMid = ['dabar jau tikrai ', ' jeigu jūs klaustumėte', 'bet jau į generalinius klausimus ', 'tai jau tikrai '];
+mod.coolMid = ['dabar jau tikrai ', 'jeigu jūs klaustumėte', 'bet jau į generalinius klausimus ', 'tai jau tikrai ', 'aš irgi paskambinau TAI moksleivei, ko pasekoje '];
 
-mod.coolOutro = ['eksportas pasiekė prieškrizinį lygį', ' mediena nugalėtų nelygioj kovoj', 'turiu trumpų ir šmaikščių atsakymų', 'niekados nepasakysiu', ' pereiname prie biokuro', 'telefonas išsikrauna', 'viskas, trūksta kantrybė'];
+mod.coolOutro = ['eksportas pasiekė prieškrizinį lygį', 'mediena nugalėtų nelygioj kovoj', 'turiu trumpų ir šmaikščių atsakymų', 'niekados nepasakysiu', 'pereiname prie biokuro', 'telefonas išsikrauna', 'viskas, trūksta kantrybė', 'atsakius sugadinčiau jums šventę'];
 
 mod.zenklas = ['?', '.', '!', '...'];
 
 mod.engIntro = ['Ai em šuje:', 'Die gesc', 'Leidyz end džentelmen', 'Disingiš gesc', 'Ai vud laik tju emfazaiz zet'];
 
-mod.engSomething = [' de komitment of ze Ripablik of Litiujiėnija iz ', ' ai em seitan ', ' de konstitušen of jūnesko '];
+mod.engSomething = [' de komitment of ze Ripablik of Litiujiėnija iz ', 'ai em seitan ', 'de konstitušen of jūnesko '];
 
 mod.engFatality = ['a šied vju', 'šoude tu šoude', 'of ze hjuman kaind', 'bete, smate, mo kriėitiv end chohierint fjūčia togezia'];
 
-mod.engOutro = ['. Fenk jū veri mač.', ' for it tiu bylong lasting.', ', pliz šier end laik.'];
+mod.engOutro = ['. Fenk jū veri mač.', 'for it tiu bylong lasting.', ', pliz šier end laik.'];
 
 mod.zoidberg1 = ['i`m swelling with patriotic mucus! ', 'You’ll never guess where I’ve been! ', 'Ah, there’s nothing like the first cup of dumpster juice in the morning!', 'Why always the fighting? ', 'You seem malnourished. Are you suffering from internal parasites?', 'I`m having a wonderfulime', 'Eexcuse me, but who\'s the doctor here?'];
 
-mod.zoidberg2 = ['By the way, I took the liberty of fertilizing your caviar', 'I’m looking for something beautiful and cheap for a lady who is one of those things!', 'Hooray, I’m useful!', 'Whoop! Whoop! Whoop!', 'Whoop! Whoop! Whoop!', ''];
+mod.zoidberg2 = ['By the way, I took the liberty of fertilizing your caviar', 'I’m looking for something beautiful and cheap for a lady who is one of those things!', 'Hooray, I’m useful!', 'Whoop! Whoop! Whoop!', 'Whoop! Whoop! Whoop!'];
 
 mod.grazulioPriesas = ['homoseksualistai', 'STT', 'STT agentai', 'seimo nariai', 'amerikos šnipas', 'konservatoriai'];
 
-mod.vytas = ['ubagai ', 'valdže ', 'vagiai ', 'šleževičius ', 'prie ruso ', 'darbo turėjom anksčiau ', 'odabar kas ', 'kače ', 'nu ', 'nu ', 'ponai ir ubagai ', 'VON nato ', 'lietuva ', 'Lietuvą ', 'durnas buvau nevogiau ', 'jau tada sakiau', 'kaimas išleikė Lietuvybe ', 'debilizmas ', 'skursime kol amerikos pančiai bus ', 'buožės ', 'negaliu kentėt ', 'galiu kentėt kiek tik reikia ', 'kenčiu ir kenčiu ', 'partijos visos parsidavė', 'niekad neparsidaviau', 'pinigus skurdžiams ', 'galiu pasakyt viskas bloges ', 'velniop užsienio bankus ', 'nachui ', 'kaimo debilai', 'ne durniam isvest Lietuva is skurdo', 'sudrebetu visa sistema ', ' ,, ', ' .', 'įrodymai kur', 'nuimkit arba mes nuplėšim ', 'galim pasiekt duodant į galvą ', 'liberalai apsišiko ', ' vagys ', 'eina į seima vogt ', 'aš būčiau mokėjęs vogt dabar viską turėčiau ', 'per visas televizijas prasau pasakyti ', 'kaip vagia ', 'smerkia ', 'kaltina ', 'engia ', 'rusija ', 'putinas vyras ', 'rusai isgelbės'];
+mod.vytas = ['ubagai ', 'valdže ', 'vagiai ', 'šleževičius ', 'prie ruso ', 'darbo turėjom anksčiau ', 'odabar kas ', 'kače ', 'nu ', 'nu ', 'ponai ir ubagai ', 'VON nato ', 'lietuva ', 'Lietuvą ', 'durnas buvau nevogiau ', 'jau tada sakiau', 'kaimas išleikė Lietuvybe ', 'debilizmas ', 'skursime kol amerikos pančiai bus ', 'buožės ', 'negaliu kentėt ', 'galiu kentėt kiek tik reikia ', 'kenčiu ir kenčiu ', 'partijos visos parsidavė', 'niekad neparsidaviau', 'pinigus skurdžiams ', 'galiu pasakyt viskas bloges ', 'velniop užsienio bankus ', 'nachui ', 'kaimo debilai', 'ne durniam isvest Lietuva is skurdo', 'sudrebetu visa sistema ', ',, ', '.', 'įrodymai kur', 'nuimkit arba mes nuplėšim ', 'galim pasiekt duodant į galvą ', 'liberalai apsišiko ', 'vagys ', 'eina į seima vogt ', 'aš būčiau mokėjęs vogt dabar viską turėčiau ', 'per visas televizijas prasau pasakyti ', 'kaip vagia ', 'smerkia ', 'kaltina ', 'engia ', 'rusija ', 'putinas vyras ', 'rusai isgelbės'];
 
 ////////////////////////////////////////////////////////////
 
-mod.akademinisPareiskimas = ['Kas yra verslas, '];
+mod.akademinisPareiskimas = ['Disertacijoje buvo ne kartą minimas našumas.', 'Gerai žinomas faktas, kad šviesa yra dalelės.', 'Šviesa, be abejo, yra bangos.', 'Fazės moduliacija disertanto pernelyg dažnai linksniuojama.', 'Išvadose pasigedau konkretumo.', 'Gynimo metu nieko nesimatė skaidrėse', 'Dar neteko dirbti su dr. Ignu.', 'Kyla abejoniu dėl dr. Jono kompetencijų.', 'Bandžiau skaityti abi disertacijas.', 'Japonijoje panašūs rezultatai jau buvo publikuojami prieš dešimtmetį.'];
 
-mod.akademinisKomentaras = ['eksportas pasiekė prieškrizinį lygį'];
+mod.akademinisKomentaras = ['Ką disertantas konkrečiai tuo norėjo pasakyti?', 'Tačiau pasigendu mokslinio naujumo.', 'Akademinė bendruomenė tam nepritars.', 'Na, projektą rašyti jau galima!', 'Silpna pradžia, žiūrėsim kaip toliau', 'Horodničiaus mokykla, vienareikšmiškai', 'Geriau jau būtų nanodalelės', 'Todėl pažeidžiami anglų kalbos pagrindai', 'Skirtumų tarp pirmo ir antro - nerasta', 'Kuom pagrįstas toks absurdas?', 'Fantastiški rezultatai, puikus darbas.', 'Tikrų tikriausiais mokslas!', 'Tikra ašarų pakalnė...', 'Belieka apgailestauti, kad tuo buvo nustatyta nauja kartelė būsimiems doktorantams.'];
+
+mod.aprielinksnis = ['Mano', 'Tavo', 'XXX Senukų', 'Augustino Rakausko', 'Dvasios verslumo', 'Dangiškojo pienių (p)ūko', 'Mūsų visų', 'Kiekvieno ir niekieno'];
+
+mod.aveiksnys = ['meilės vienybės harmonija', 'išminties intelektas', 'teisingumo potyris', 'tikėjimo septyniukė', 'meilės filharmonija', 'vienybės meilė', 'jausmo proto būsena', 'jausmo protena', 'proto jausmena', 'dvasios USB laikmena'];
+
+mod.aoptional = ['Augustino', '', '', 'aštuoniukės', 'dvasios proto', 'sielos jausmens', 'Rakausko'];
+
+mod.aveiksnys2 = ['turtus', 'syvus', 'sveikatą', 'džiaugsmą', 'gėrį', 'blogį', 'gyvenimo skonį'];
+
+mod.atarinys = ['turėti', 'skleisti', 'išsunkti', 'pasigvožti', 'demonstruoti', 'taškyti'];
+
+mod.aveiksnys3 = ['Tikslas', 'Senukų prakeiksmas', 'Visiškas ištvirkimas', 'Akibrokštas', 'Gaivališkas aštuonetas', 'Verslumo dvasia'];
+
+mod.abendratis = ['sugebėti suprasti', 'žmonijos gaivalui nušviesti', 'nuteikti', 'įpykusiai miniai parodyti', 'sumenkinti', 'nutukinti', 'atbukinti', 'materializuoti', 'apmokęstinti'];
+
+mod.apezmuo = ['mano imperiją', 'jausmo protą', 'harmonijos aušrą', 'minties kvadratą', 'elipsės proto radiusą', 'jausmo tranformacijos perturbacijas', 'mano paties proto jausmą', 'emocinę devyniukę', 'daugiaryžį'];
+
+mod.AZintro = ['Miela', 'Gerbiama', 'Milijonine', 'Akademine', 'Projektine'];
+
+mod.AZveiksnys = ['publika', 'bendruomene', 'aukštuomene', 'draugija', 'akademija'];
+
+mod.AZtarinys = ['dalyvaukime orientaciniame bėgime!', 'linksminkimės iki sekmadienio!', 'visas disertacijas švęskime tik šitaip!', 'kas išjungė šviesą Žindulio rūsyje?','pakvieskite man daktarą (ne Tą)!!!', 'uždarykime daktarą (Tą)!', 'dalyvaukime milijone projektų!', 'dalis universitetų Lietuvoje - Гиблое дело', 'viešieji pirkimai - чяпуха', 'vienas milijonas - мелочь', 'milijonas milijonų - slėnio projektas', 'kas matė Redą?', 'kas ištarabildino projektinį finansavimą?'];
 
 module.exports = mod;
